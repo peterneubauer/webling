d3e12723f83654f7c46749e2f707704ada03bb3e
Pavel Yaskevich <povel.y@gmail.com>
2010-03-28 19:31:03 +0300

    last tutorial sections.

--- public/javascripts/tutorial.js
+++ public/javascripts/tutorial.js
@@ -6,9 +6,10 @@ var Tutorial = function() {
 Tutorial.prototype = {
   index: function() {
       this.next = this._p1;
-      return P('Use `tutorial &lt;chapter&gt; &lt;section&gt;`') + 
+      return P('Use `tutorial &lt;chapter&gt; &lt;section&gt;` or `next` `prev` commands to navigate.') + BR() +
           P('Available sections are:') +
-          DIV(LIST(['1. What is Gremlin?', 
+          DIV(LIST([
+                     '1. What is Gremlin?', 
                      '2. What can I do with Gremlin?' +
                         LIST([
                           'a. Defining variables.',
@@ -16,9 +17,7 @@ Tutorial.prototype = {
                           'c. Using Gremlin build-in functions and data structures (maps, lists).',
                           'd. Gremlin Loops (foreach, while).',
                           'e. Defining custom functions/paths.',
-                          'f. Basic graph traversals (including load graph data from files).',
-                          'g. Using different backends (Neo4j, TinkerGraph, Sail).',
-                          'h. Spreading Activation algorithm.'
+                          'f. Basic graph traversals.'
                         ]), 
                      '3. Acknowledgements'
                     ]));
@@ -105,7 +104,7 @@ Tutorial.prototype = {
 
     this._d = function() {
       this.prev = this._c;
-      this.next = this._p3;
+      this.next = this._e;
 
       return P("Chapter 2d - Gremlin Loops (foreach, while).") + BR() +
              P("&nbsp;Gremlin also has build-in loop support - foreach and while:") + BR() +
@@ -129,13 +128,95 @@ Tutorial.prototype = {
              P("this will return - 'ggg'");
     }
 
+    this._e = function() {
+      this.prev = this._d;
+      this.next = this._f;
+
+      return P("Chapter 2e - Defining custom functions/paths.") + BR() +
+             P("&nbsp;Gremlin gives you possibility to define custom functions and paths.") + BR() + 
+             P("Function can be defined using following syntax:") + BR() +
+             PLIST("func &lt;prefix&gt;:&lt;functiona-name&gt;($var, ...)<br/>&nbsp;&nbsp;statement*<br/>end") + 
+             P("Example") + BR() +
+             PLIST("func u:hello-name($name)<br/>&nbsp;&nbsp;g:print(concat('hello ', $name))<br/>end") + BR() +
+             P("and if you then run `u:hello-name('pavel')` result will be 'hello pavel'.") + 
+             P("Please note - there are no return statement - function returns value of its last statement!") + BR() +
+             P("Path can be defined using following syntax:") + BR() + 
+             PLIST("path string<br/>&nbsp;&nbsp;statement*<br/>end") + BR() +
+             P("Example") + BR() +
+             PLIST("path co-developer<br/>&nbsp;&nbsp;./outE[@label='created']/inV/inE[@label='created']/outV[g:except($_)]<br/>end");
+    }
+
+    this._f = function() {
+      this.prev = this._e;
+      this.next = this._p3;
+
+      return P("Chapter 2f - Basic graph traversals.") + BR() + 
+             P("&nbsp;First of all we should learn how to open graph and how to load graph data from file.") +
+             P("As Gremlin has alot of backends graph could be opened using different functions, most common if then are:") + BR() +
+             PLIST("1. tg:open() - used to open TinkerGraph;") + 
+             PLIST("2. neo4j:open(&lt;database-name-as-string&gt;) - used to open Neo4j database connection;") + 
+             PLIST("3. sail:open() - used to open SAIL memorystore.") + BR() +
+             P("Each of those functions return unified graph object which could be assigned to a variable. Gremlin has special variable for this aim - '$_g'. When we have your graph opened next thing to do is to set root list to '$_' variable (if there are any existing vertices in the graph):") + BR() +
+             PLIST("$_ := g:id('1') - function g:id(graph?, id) used to get vertex from graph object by its id. If you have $_g variable assigned then you don't have to provide graph? argument to the g:id function.") + 
+             ("After $_ is set you can access your root list by using '.' statement") + BR() +
+             P("Here is the list of most used functions to work with graphs:") + BR() +
+             PLIST("1. vertex g:id(graph?, id) - get vertex by id.") +
+             PLIST("2. vertex g:add-v(graph?, object?, object?) - add new vertex with given attributes, e.g. g:add-v($g, g:map('name', 'pavel')).") +
+             PLIST("3. edge g:add-e(graph?, object?, vertex, string, vertex) - add new edge connecting to vertexes, e.g. g:add-e($g, $v1, 'knows', $v2).") +
+             PLIST("4. boolean g:save(string) - save graph data into XML file with given name, e.g. g:save('my-graph').") + 
+             PLIST("5. boolean g:load(graph?, string) - load graph data from XML file with given name to graph object variable, e.g. g:load($g, 'my-graph').") +
+             PLIST("6. boolean g:clear(graph?) - clear contents of the graph, e.g. g:clear($g).") + 
+             PLIST("7. boolean g:close(graph?) - close given graph.") + BR() +
+             P("Lets now make a simple graph traversal:") + BR() + 
+             PLIST("# open a graph object") + 
+             PLIST("$_g := tg:open()") + BR() +
+             PLIST("# assign root list to a new vertex") +
+             PLIST("$_ := g:add-v()") + BR() +
+             PLIST("# assign @name attribute to the root vertex") + 
+             PLIST("./@name := 'pavel'") + BR() +
+             PLIST("# create new vertex and assign it to $marko variable") + 
+             PLIST("$marko := g:add-v()") + BR() + 
+             PLIST("# add @name attribute as we did before") +
+             PLIST("$marko/@name := 'marko'") + BR() +
+             PLIST("# add edge to connect pavel and marko") + 
+             PLIST("g:add-e($_, 'knows', $marko)") + BR() +
+             PLIST("# lets traverse our graph from root vertex to find out outgoing edges") + 
+             PLIST("./outE") + BR() +
+             PLIST("# lets get ingoing vertexs to your outgoing edge") +
+             PLIST("./outE/inV") + BR() + 
+             PLIST("# let get a @name attribute of found vertex") +
+             PLIST("./outE/inV/@name") + BR() +
+             PLIST("# lets add few more vertices and search by criteria") + 
+             PLIST("$max := g:add-v()") +
+             PLIST("$max/@name := 'max'") + 
+             PLIST("$max/@age := 23") +
+             PLIST("$liza := g:add-v()") +
+             PLIST("$liza/@name := 'liza'") +
+             PLIST("$liza/@age := 15") +
+             PLIST("g:add-e($_, 'knows', $max'") +
+             PLIST("g:add-e($_, 'has syster', $liza)") +
+             PLIST("# filter by 'knows' edges") + 
+             PLIST("./outE[@label = 'knows']/inV/@name") +
+             PLIST("# all outgoing edges with label 'knows' and age more then 20 years") + 
+             PLIST("./outE[@label = 'knows']/inV[@age > 20]/@name") + BR() +
+             PLIST("# lets save and close the graph and then load data from file") +
+             PLIST("g:save('my-graph')") +
+             PLIST("g:close()") +
+             PLIST("g:load('my-graph')") + BR() +
+             P("For bigger example please visit " + LINK("http://wiki.github.com/tinkerpop/gremlin/basic-graph-traversals", "wiki")); 
+    }
+
     var section = this['_' + sectionId];
     return ((section) ? section() : this._annotation());
   },
 
   _p3: function() {
+    this.prev = this._p2;
     this.next = this._p1;
-    return P("Chapter 3");
+    return P("Chapter 3 - Acknowledgements.") + BR() +
+           P("Special thanks to:") + BR() +
+           PLIST(LINK("http://www.linkedin.com/in/neubauer", "Peter Neubauer")) + 
+           PLIST(LINK("http://markorodriguez.com/", "Marko A. Rodriguez"));
   },
 
   handle: function(req) {
